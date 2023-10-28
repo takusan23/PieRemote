e6aba30c375d975eb12b3a9af5661bab3c4927cf
takusan23 <takumu1200@gmail.com>
2023-10-28 15:56:54 +0900

    古い情報が出続ける問題を修正

--- app/src/main/java/io/github/takusan23/pieremote/MusicNotificationListener.kt
+++ app/src/main/java/io/github/takusan23/pieremote/MusicNotificationListener.kt
@@ -3,14 +3,15 @@ package io.github.takusan23.pieremote
 import android.content.ComponentName
 import android.content.Context
 import android.media.MediaMetadata
+import android.media.session.MediaController
 import android.media.session.MediaSession
 import android.media.session.MediaSessionManager
 import android.media.session.PlaybackState
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import io.github.takusan23.pieremotecommon.DataSyncTool
-import io.github.takusan23.pieremotecommon.data.MusicControlEvent
 import io.github.takusan23.pieremotecommon.WearMessageTool
+import io.github.takusan23.pieremotecommon.data.MusicControlEvent
 import io.github.takusan23.pieremotecommon.data.MusicItem
 import io.github.takusan23.pieremotecommon.data.PieRemoteData
 import kotlinx.coroutines.CoroutineScope
@@ -24,28 +25,28 @@ class MusicNotificationListener : NotificationListenerService() {
     private val scope = CoroutineScope(Dispatchers.Main + Job())
 
     /** よその音楽アプリと連携するために MediaSession を利用する */
-    private val mediaSession by lazy {
-        // MediaSession取得
-        val mediaSessionManager = getSystemService(Context.MEDIA_SESSION_SERVICE) as MediaSessionManager
-        val activeMediaSessionList = mediaSessionManager.getActiveSessions(ComponentName(this, MusicNotificationListener::class.java))
-        activeMediaSessionList.firstOrNull()
-    }
+    private val mediaController: MediaController?
+        get() {
+            // MediaSession取得
+            val mediaSessionManager = getSystemService(Context.MEDIA_SESSION_SERVICE) as MediaSessionManager
+            val activeMediaSessionList = mediaSessionManager.getActiveSessions(ComponentName(this, MusicNotificationListener::class.java))
+            return activeMediaSessionList.firstOrNull()
+        }
 
     /** 初期化 */
     override fun onCreate() {
         super.onCreate()
-
-        val mediaSession = mediaSession ?: return
-
         // WearOS から来るイベントが Flow で通知されるので監視する
         scope.launch {
             WearMessageTool.receiveMusicControlEvent(this@MusicNotificationListener).collect { musicControlEvent ->
+                val mediaController = mediaController ?: return@collect
+
                 when (musicControlEvent) {
-                    is MusicControlEvent.Play -> mediaSession.transportControls.play()
-                    is MusicControlEvent.Pause -> mediaSession.transportControls.pause()
-                    is MusicControlEvent.Next -> mediaSession.transportControls.skipToNext()
-                    is MusicControlEvent.Prev -> mediaSession.transportControls.skipToPrevious()
-                    is MusicControlEvent.MoveIndex -> mediaSession.transportControls.skipToQueueItem(musicControlEvent.mediaId)
+                    is MusicControlEvent.Play -> mediaController.transportControls.play()
+                    is MusicControlEvent.Pause -> mediaController.transportControls.pause()
+                    is MusicControlEvent.Next -> mediaController.transportControls.skipToNext()
+                    is MusicControlEvent.Prev -> mediaController.transportControls.skipToPrevious()
+                    is MusicControlEvent.MoveIndex -> mediaController.transportControls.skipToQueueItem(musicControlEvent.mediaId)
                 }
             }
         }
@@ -61,15 +62,15 @@ class MusicNotificationListener : NotificationListenerService() {
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         super.onNotificationPosted(sbn)
 
-        val mediaSession = mediaSession ?: return
+        val mediaController = mediaController ?: return
 
         // 音楽アプリの状態を WearOS の PieRemote へ送る
         scope.launch {
-            val currentPlayingItem = mediaSession.queue
-                ?.first { it.description.title == mediaSession.metadata?.getString(MediaMetadata.METADATA_KEY_TITLE) }
+            val currentPlayingItem = mediaController.queue
+                ?.first { it.description.title == mediaController.metadata?.getString(MediaMetadata.METADATA_KEY_TITLE) }
                 ?.toMusicItem() ?: return@launch
-            val queueItem = mediaSession.queue?.map { it.toMusicItem() } ?: emptyList()
-            val isPlaying = mediaSession.playbackState?.state == PlaybackState.STATE_PLAYING
+            val queueItem = mediaController.queue?.map { it.toMusicItem() } ?: emptyList()
+            val isPlaying = mediaController.playbackState?.state == PlaybackState.STATE_PLAYING
 
             DataSyncTool.sendPieRemoteData(
                 context = this@MusicNotificationListener,
